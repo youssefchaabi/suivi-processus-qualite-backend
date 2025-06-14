6c2446e4657b1a4ec8cad93c78fac484b1e00c1b
youssef chaabi <chaabiy214@gmail.com>
2025-06-14 14:05:49 +0100

    verife

--- src/main/java/com/pfe/qualite/backend/security/config/SecurityConfig.java
+++ src/main/java/com/pfe/qualite/backend/security/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login").permitAll()
-                        .requestMatchers("api/utilisateurs").authenticated()
+                        .requestMatchers("api/utilisateurs").authenticated()  // a verifier
                         .anyRequest().authenticated()
                 );
 
